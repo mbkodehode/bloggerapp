350116e794f7346c1a1ce449236dba6258b637ff
mbkodehode <mb1811kodehode@gmail.com>
2025-03-10 13:32:31 +0100

    Add Entity Framework Core setup and JSON options configuration

--- blogV1/Program.cs
+++ blogV1/Program.cs
@@ -1,5 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//register db context 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+//inorder to stop json cyle 
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true; // Pretty JSON output 
+
+});
+
 var app = builder.Build();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
